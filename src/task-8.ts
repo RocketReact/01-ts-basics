1f47a334b09395dd9457056de0d8b0af8f81187d
Oleg Shapoval <olegkazhan@gmail.com>
2025-08-03 23:04:13 +0300

    done 8 tasks

---
+++ src/task-8.ts
@@ -0,0 +1,26 @@
+import axios from 'axios';
+
+interface Post {
+  id: number;
+  title: string;
+  body: string;
+}
+async function fetchPosts(): Promise<Post[]> {
+  try {
+    const response = await axios.get<Post[]>(
+      'https://jsonplaceholder.typicode.com/posts'
+    );
+    return response.data;
+  } catch (error) {
+    console.error(error);
+    return [];
+  }
+}
+
+fetchPosts().then(posts => {
+  if (posts.length > 0) {
+    console.log(posts[0].title);
+  } else {
+    console.log('No posts have been found');
+  }
+});
